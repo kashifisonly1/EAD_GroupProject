32c19729eba9947ce8ef0e45351954c46dc5a4a7
kashifisonly1 <kashift99@gmail.com>
2021-05-04 02:14:56 +0500

    Let's Begin Working

--- EAD_Project_Group_3/EAD_Project_Group_3/Controllers/HomeController.cs
+++ EAD_Project_Group_3/EAD_Project_Group_3/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using EAD_Project_Group_3.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -17,21 +16,9 @@ namespace EAD_Project_Group_3.Controllers
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ EAD_Project_Group_3/EAD_Project_Group_3/Models/BussinessObjects/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EAD_Project_Group_3.Models.BussinessObjects
+{
+    public class Class
+    {
+    }
+}
