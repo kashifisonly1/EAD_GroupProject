e8fce9f6cd035a0ff2c85c6226c56c01035d07a6
Usama Aziz <usamaaziz999@gmail.com>
2021-05-05 12:02:14 +0500

    Added additional attributes in ApplicationUser Model

--- JobPortal.WebUI/Areas/Identity/Data/ApplicationUser.cs
+++ JobPortal.WebUI/Areas/Identity/Data/ApplicationUser.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Identity;
 
 namespace JobPortal.WebUI.Areas.Identity.Data
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
-    {
-    }
+	// Add profile data for application users by adding properties to the ApplicationUser class
+	public class ApplicationUser : IdentityUser
+	{
+		[Required]
+		[PersonalData]
+		[Column(TypeName = "NVARCHAR(50)")]
+		public string FullName { get; set; }
+
+		[Required]
+		[PersonalData]
+		[Column(TypeName = "NVARCHAR(50)")]
+		public string Role { get; set; }
+
+		[Column(TypeName = "datetime2(7)")]
+		public DateTime DateCreated { get; set; } = DateTime.Now;
+
+		[Required]
+		[PersonalData]
+		[Column(TypeName = "varbinary(max)")]
+		public byte[] ProfileImage { get; set; }
+
+	}
 }
