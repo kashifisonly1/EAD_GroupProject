83327c0767b4b5708b8a5d28fdcce49b46821daf
kashif tariq <kashift99@gmail.com>
2021-05-07 17:02:50 +0500

    adding admin panel

---
+++ JobPortal.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using JobPortal.WebUI.Models;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JobPortal.WebUI.Controllers
+{
+	//[Authorize]
+	public class AdminController : Controller
+	{
+		private readonly ILogger<AdminController> _logger;
+
+		public AdminController(ILogger<AdminController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Category()
+		{
+			return View();
+		}
+		public IActionResult User()
+		{
+			return View();
+		}
+		public IActionResult ContactUs(ContactForm form)
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
