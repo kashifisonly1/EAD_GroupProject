eb0fc67c08abb657d5ef91aa338413b4209291bb
Usama Aziz <usamaaziz999@gmail.com>
2021-05-09 23:14:17 +0500

    Updated WebApi to accept/deliver ContactUsObject.

---
+++ JobPortal.Api/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using JobPortal.DataManager.Data;
+using JobPortal.WebUI.Library.Api.Models;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace JobPortal.Api.Controllers
+{
+	//[Authorize]
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AdminController : ControllerBase
+	{
+		// GET: api/<AdminController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			return new string[] { "value1", "value2" };
+		}
+
+		//[Authorize(Roles = ("Admin, Client"))]
+		[Route("/postmessage")]
+		[HttpPost]
+		public void PostContactUsMessage(ContactUsModel model)
+		{
+			JobPortal.DataManager.Models.ContactUsModel contact = new DataManager.Models.ContactUsModel()
+			{
+				Email = model.Email,
+				Subject = model.Subject,
+				Message = model.Message,
+				Name = model.Name
+			};
+			ContactUsData data = new ContactUsData();
+			data.SaveMessage(contact);
+		}
+	}
+}
--- JobPortal.Api/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ JobPortal.Api/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,29 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "E:\Semester6\EAD\EAD_GroupProject\JobPortal.Api\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "E:\Semester6\EAD\EAD_GroupProject\JobPortal.Api\Areas\Identity\Pages\_ViewImports.cshtml"
+using JobPortal.Api.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "E:\Semester6\EAD\EAD_GroupProject\JobPortal.Api\Areas\Identity\Pages\_ViewImports.cshtml"
+using JobPortal.Api.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"13b82ef97968a56a4de533f67cf3e21d662f41aa", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"33412a8ece1f51a0e563f93f0397c22954f79c35", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
