51c8a2c5ba9d08a2aeafa4eb0542ca5571681dc8
Saam J. Z3R0 <bsef18m038@pucit.edu.pk>
2021-05-20 22:18:34 +0500

    Add card View in addition to List View

--- JobPortal.WebUI/Controllers/ClientController.cs
+++ JobPortal.WebUI/Controllers/ClientController.cs
@@ -19,11 +19,36 @@ namespace JobPortal.WebUI.Controllers
             return View("PurchaseRequest");
         }
 
+        public IActionResult PurchaseRequestCard() 
+        {
+
+            //TODO:  handle PurchaseRequestForm data to store in database
+            List<PurchaseRequestForm> list = new List<PurchaseRequestForm>();
+            for(int i=0; i<4;i++)
+            {
+                PurchaseRequestForm req = new PurchaseRequestForm { RequestID = 1, RequestNamer = "Alpha", RequestDescription = "I need a Logo designed by a Vue!", RequestOfferCount = 1, RequestDuration = 2, RequestBudget = 5.0, RequestDate = System.DateTime.Now.Date };
+                list.Add(req);
+
+            }
+            ViewData["request-view-card"] = list;
+            return View("PurchaseRequestCard");
+
+        }
+
+        public void RequestHandler()
+        {
+
+            //TODO:  handler for bid invites  
+
+            Console.WriteLine("Handle Stuff");
+        }
+
+
         public IActionResult PurchaseRequest()
         {
             //TODO:  handle PurchaseRequestForm data to store in database
             List<PurchaseRequestForm> list = new List<PurchaseRequestForm>();
-           PurchaseRequestForm  req = new PurchaseRequestForm { RequestID = 1, RequestNamer = "Alpha", RequestDescription = "I need a Logo designed by a Vue!", RequestOfferCount = 1, RequestDuration = 2, RequestBudget = 5.0, RequestDate = System.DateTime.Now.Date };
+           PurchaseRequestForm  req = new PurchaseRequestForm { RequestID = 1, RequestNamer = "Alpha", RequestDescription = "I need a Logo designed by a Vue!", RequestOfferCount = 1, RequestDuration = 2, RequestBudget = 5.0, RequestDate = System.DateTime.Now.Date , RequestCategory ="Design" };
             list.Add(req);
 
             ViewData["purchase-request"] = list;
