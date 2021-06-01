51da9b47bea56e7085564c9c3fd91168e1a112fe
saimbintalib <49138179+saimbintalib@users.noreply.github.com>
2021-06-02 01:27:35 +0500

    Category End Points

---
+++ JobPortal.WebUI.Library/Api/EndPoints/CategoryEndPoints.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using JobPortal.BusinessModels.Gigs;
+
+namespace JobPortal.WebUI.Library.Api.EndPoints
+{
+    public class CategoryEndPoints
+    {
+        ApiHelper ApiHelper;
+        public CategoryEndPoints()
+        {
+            ApiHelper = new ApiHelper();
+        }
+        public async Task<List<Category>> GetAllCategories()
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync("/api/Categories"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<Category>>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        public async Task<Category> GetCategory(int id)
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync($"/api/Categories/{id}"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<Category>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        public async Task AddCategory(Category category)
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync("/api/Categories", category))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        public async Task UpdateCategory(int id)
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync("/api/Categories",id))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }    
+        }
+        public async Task DeleteCategory(int id)
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync("/api/Categories", id))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ JobPortal.WebUI.Library/Api/EndPoints/CustomOrderRequestsEndPoints.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using JobPortal.BusinessModels.Client;
+
+namespace JobPortal.WebUI.Library.Api.EndPoints
+{
+    public class CustomOrderRequestsEndPoints
+    {
+        ApiHelper ApiHelper;
+        public CustomOrderRequestsEndPoints()
+        {
+            ApiHelper = new ApiHelper();
+        }
+        public async Task<List<CustomOrderRequest>> GetCustomOrderRequests()
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync("/api/CustomOrderRequests"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<List<CustomOrderRequest>>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+                
+            }
+        }
+        public async Task<CustomOrderRequest> GetCustomOrderRequest(int id)
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync($"/api/CustomOrderRequests/{id}"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<CustomOrderRequest>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        public async Task AddCustomOrderRequest(CustomOrderRequest customOrderRequest)
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync("api/CustomOrderRequests",customOrderRequest))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        public async Task UpdateCustomOrderRequest(int id)
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync($"/api/CustomOrderRequests",id))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+        public async Task DeleteCustomOrderRequest(int id)
+        {
+            ApiHelper.ApiClient.DefaultRequestHeaders.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiHelper.ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync("/api/CustomOrderRequests", id))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
